ba6596daf14576b34e52e035a6e80de017004de1
mr59 <Kralboubouk59>
2020-10-13 00:56:02 +0200

    Functions

--- src/main/scala/com/hei/fp/project/Functions.scala
+++ src/main/scala/com/hei/fp/project/Functions.scala
@@ -1,12 +1,50 @@
 package com.hei.fp.project
 
+import scala.annotation.tailrec
+import scala.math.Numeric.BigDecimalAsIfIntegral.mkOrderingOps
+
+// A comment!
+/* Another comment */
+/** A documentation comment */
+
 class Functions extends Utils {
 
+  var x = 12 // variable mutable
+  val y = 14 // variable immutable - y sera évalué à l'execution, une fois pour toutes.
+  def z = 14 // la partie droite sera évaluée lors de l'appel de z.
+
+  /*------------ Les paramètres -----------------*/
+  def square(x : Double) : Double = x * x
+  // square: (x: Double)Double
+  // scala> square(2)
+  // res0: Double = 4.0
+  // scala> square(5 + 3)
+  // res1: Double = 64.0
+  // scala> square(square(4))
+  // res2: Double = 256.0
+
+  /**
+   * Write a function sumOfSquare that returns x^2 + y2 and test it with 3 , 2+2
+   */
+  def sumOfSquares(x: Double, y: Double) = square(x) + square(y)
+
+  /*-------------- Expressions conditionnelles : if - else ------------------------*/
+  def abs(x: Double) = if (x < 0) -x else x
+
+  //-------------- Call by name - call by value
+  def loop : Int = loop
+  //  loop: Int
+  def first(x: Int, y: Int) = x  // ----- Call by name => first(1,loop) = execution infinie
+  // first: (x: Int, y: Int)Int
+  def first(x: Int, y: => Int) = x  // ---- Call by value => first(1,loop)    res0: Int = 1
+
   // Anonymous function
-  (1 to 5).map( x => x + 1)
+  (1 to 5).map( x => x * 1)
+  ///(x: Int, y: Int) => x + y
 
-  (x: Int, y: Int) => x + y
-
+  /**
+   * Write a val function with parameters
+   */
   val s = (x:Int) => x + 1
 
   /**
@@ -23,7 +61,7 @@ class Functions extends Utils {
   def cube(x:Int) : Int = x * x * x
 
   /**
-   * Define addTwo trait function then define and test a function take a integer
+   * Define addTwo trait function then define and test a function take an integer x
    * @param x
    * @return  : x + addTwo(x) trait function
    */
@@ -37,8 +75,7 @@ class Functions extends Utils {
    */
   def addAnonymousToInt(x : Int, f:(Int) => Int ) : Int = x + f(x)
 
-
-  // todo map on array list & functions
+  // todo work on the array of list & functions applied
 
   val numbers = Array(Array(1,2,3,4,5),
     Array(6,7,8,9,10),
@@ -54,26 +91,85 @@ class Functions extends Utils {
     xs.map(x => f(x))
   }
 
-  // todo correct spelling
-  val test =  operation(numbers(0),cube)
-
-  def runAllOperation = ???
-
-  numbers
+  def runAllOperations : Unit =
+    numbers
     .flatMap(operation(_,cube))
     .foreach(println)
 
+  /*-------------- Recursivité ---------------------------------------------------*/
+  /**
+   * Write and test a function return the factorial value of an integer
+   * @param n : Int
+   * @return fact(x)
+   */
+  def factorielle(n : Int) : Int = {
+    if (n == 1) 1
+    else n * factorielle(n - 1)
+  }
 
-  // todo example with implicits
+  /*------------- Calcul des la racine carrée par la méthode de Newton ------------*/
+  // Objectif : définir la fonction
+  // def sqrt(x:Double) : Double = ???
 
-  // todo define high oder function
 
   // todo functions imbriquées : racines carrées par la methode de Newton
+  // Algorithme y = sqrt(x)
+  // On part d'une approximation de y => par exemple y  = 1 pour la racine carrée de 2
+  // Puis on améliore à chaque itération en prenant la valeur (nouvelle estimation) de (y + x/y)/2
+  //  y     x/y       (y + x/y)/2
+  //  1     2/1        1.5
+  //  1.5   2/1.5      1.4167    ....
 
-  // todo recursive : go to gcd : Fibionacci  -> trait gcd
+  def sqrtIter(guess: Double, x: Double): Double =
+    if (isGoodEnough(guess, x)) guess
+    else sqrtIter(improve(guess, x), x)
+
+  val e : Double = 0.001
+  def improve(guess: Double, x: Double) : Double = (guess + x / guess) / 2
+  def isGoodEnough(guess: Double, x: Double): Boolean = abs(square(guess) - x) < e
+
+  def sqrt(x: Double) = sqrtIter(1.0, x)
+
+
+   // todo rewrite the function sqrt(x) with only one block -  What about the parameters ? Can we simplify ?
+   def sqrt = ???
+
+
+  /* ----------- recursivité terminale ------------------*/
+  def gcd(a :Int, b : Int) : Int =
+    if (b == 0 ) a
+    else gcd(b, a % b)
+
+  // gcd(14, 21)
+  // if (21 == 0) 14 else gcd(21, 14 % 21) if (false) 14 else gcd(21, 14 % 21) gcd(21, 14 % 21)
+  // gcd(21, 14)
+  // if (14 == 0) 21 else gcd(14, 21 % 14) gcd(14, 21 % 14)
+  // gcd(14, 7)
+  // if (7==0) 14 else gcd(7, 14 % 7) gcd(7,14 % 7)
+  // gcd(7, 0)
+  // if(0 = =0) 7 else gcd(0, 7 % 0)
+  // 7
+
+  // -> todo compare with the last factorielle formulation
+  // -> todo write a tailrec factorielle function
+
+ def fact(acc : Int, n : Int) : Int = {
+      if (n <= 1) acc
+      else fact(n * acc, n - 1)
+  }
 
   // todo currying
 
+
+
+  /**
+   * Write a functions
+   * @param l : List[Int]
+   * @return : sum of list elements
+   */
+  def foldSum(l : List[Int]) = {
+    l.foldLeft(0)(_ + _)
+  }
 
 
 }
